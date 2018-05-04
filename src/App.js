e60bfbf870d18172d03bcd6c9b9265181e69217f
mteres <teres@ufrrj.br>
2018-05-04 10:03:00 -0300

    ah, é edmundo

---
+++ src/App.js
@@ -0,0 +1,201 @@
+import React, { Component } from 'react';
+import edgol from './edmundo.jpg';
+import styled, { css } from 'styled-components';
+import Scoreboard from './Components/Scoreboard/Scoreboard';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      timer: {
+        seconds: 0,
+        started: false,
+        paused: true,
+      },
+      score: {
+        home: 0,
+        away: 0,
+        homeScoring: false,
+        awayScoring: false,
+      },
+    };
+  }
+
+  tickTimer() {
+    this.setState(prevState => ({
+      ...prevState,
+      timer: {
+        ...prevState.timer,
+        seconds: prevState.timer.seconds + 1,
+      }
+    }))
+  }
+
+  handleTimer = () => {
+    const { timer: { paused } } = this.state;
+    if (paused) {
+      this.timerID = setInterval(() => this.tickTimer(), 1000)
+    } else {
+      clearInterval(this.timerID)
+    }
+    this.setState(prevState => ({
+      ...prevState,
+      timer: {
+        ...prevState.timer,
+        started: true,
+        paused: !paused
+      }
+    }))
+  };
+
+  resetTimer = () => {
+    clearInterval(this.timerID);
+    this.setState(prevState => ({
+      ...prevState,
+      timer: {
+        seconds: 0,
+        started: false,
+        paused: true,
+      },
+      score: {
+        home: 0,
+        away: 0,
+        homeScoring: false,
+        awayScoring: false,
+      },
+    }))
+  };
+
+  handleGoal = (e) => {
+    const { team } = e.target.dataset;
+    this.goalSolver(team)
+  };
+
+  goalSolver(team) {
+    const { score } = this.state;
+    if(score[`${team}Scoring`]) {
+      this.setState(prevState => ({
+        ...prevState,
+        score: {
+          ...prevState.score,
+          [`${team}Scoring`]: false,
+          [team]: prevState.score[team] + 1
+        }
+      }));
+      clearInterval(this.goalID)
+    } else {
+      this.setState(prevState => ({
+        ...prevState,
+        score: {
+          ...prevState.score,
+          [`${team}Scoring`]: true,
+        }
+      }));
+      this.goalID = setInterval(() => this.goalSolver(team), 1000)
+    }
+  }
+
+  render() {
+    const { timer, score } = this.state;
+    const canScore = !timer.started || (score.awayScoring || score.homeScoring || timer.paused);
+    return (
+      <Main>
+        <PlayerScreen>
+          <Scoreboard timer={timer} score={score} />
+        </PlayerScreen>
+        <Controls>
+          <Section>
+            <Tittle>
+              Timer
+            </Tittle>
+            <Content>
+              <Button onClick={this.handleTimer} disabled={timer.started}>Start</Button>
+              {timer.started &&
+                <Button onClick={this.handleTimer} disabled={!timer.started}>
+                  {`${timer.paused ? 'Resume' : 'Pause'}`}
+                </Button>
+              }
+              {timer.started &&
+                <Button onClick={this.resetTimer} disabled={!timer.started}>Reset</Button>
+              }
+            </Content>
+          </Section>
+          <Section>
+            <Tittle>
+              Score
+            </Tittle>
+            <Content>
+              <Button onClick={this.handleGoal} disabled={canScore} data-team='home'>Home</Button>
+              <Button onClick={this.handleGoal} disabled={canScore} data-team='away'>Away</Button>
+            </Content>
+          </Section>
+        </Controls>
+      </Main>
+    );
+  }
+}
+
+const Main = styled.div`
+  align-items: center;
+  background-color: #30303A;
+  display: flex;
+  justify-content: space-around;
+  min-height: 100vh;
+  padding: 0 20px;
+`;
+
+const PlayerScreen = styled.div`
+  background-image: url(${edgol});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  min-height: 800px;
+  min-width: 850px;
+  position: relative;
+`;
+
+const Controls = styled.div`
+  background-color: #121214;
+  width: 300px;
+  height: 400px;
+`;
+
+const Section = styled.div`
+  border-bottom: 1px solid #fff;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Tittle = styled.h1`
+  color: #fff;
+  font-size: 2rem;
+  margin-top: 10px;
+  text-align: center;
+`;
+
+const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 10px 10px;
+`;
+
+const Button = styled.button`
+  background: #343436;
+  border-radius: 3px;
+  border: 3px solid #343436;
+  color: white;
+  cursor: pointer;
+  display: inline-block;
+  font-size: 1rem;
+  line-height: 1.2;
+  margin: 0 10px 0 0;
+  outline: 0;
+  padding: 10px 16px;
+  text-align: center;
+  ${p => p.disabled && css`
+    opacity: .6;
+    cursor: initial;
+  `}
+`;
+
+export default App;
---
+++ src/Components/Scoreboard/Scoreboard.jsx
@@ -0,0 +1,187 @@
+/* ------------------------------------------
+   Scoreboard - Component
+--------------------------------------------- */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled, { css, keyframes } from 'styled-components';
+
+class Scoreboard extends Component {
+  static displayName = 'Scoreboard';
+  static propTypes = {
+    timer: PropTypes.shape({
+      started: PropTypes.bool.isRequired,
+      seconds: PropTypes.number.isRequired,
+    }).isRequired,
+    teamHome: PropTypes.shape({
+      abbreviation: PropTypes.string.isRequired,
+      color: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    }).isRequired,
+    teamAway: PropTypes.shape({
+      abbreviation: PropTypes.string.isRequired,
+      color: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    }).isRequired,
+    score: PropTypes.shape({
+      home: PropTypes.number.isRequired,
+      away: PropTypes.number.isRequired,
+      homeScoring: PropTypes.bool.isRequired,
+      awayScoring: PropTypes.bool.isRequired,
+    }).isRequired,
+  };
+  render() {
+    const {
+      timer: { started, seconds },
+      score: {home, away, homeScoring, awayScoring}
+    } = this.props;
+    const m = Math.floor(seconds/60);
+    const s = seconds - m * 60;
+    const time = `${m >= 10 ? m : `0${m}`}:${s >= 10 ? s : `0${s}`}`;
+    return (
+      <Main>
+        <Team>
+          <TeamColor color={'white'} />
+          <Name>VAS</Name>
+        </Team>
+        <ScoreBox>
+          <ScoreContainer>
+            <Score>
+              {homeScoring
+                ?
+                (<Score>
+                  <ScoreSpan in>{home + 1}</ScoreSpan>
+                  <ScoreSpan out>{home}</ScoreSpan>
+                </Score>)
+                : (<ScoreSpan>{home}</ScoreSpan>)
+              }
+            </Score>
+          </ScoreContainer>
+          <ScoreContainer>
+            <Score>
+              {awayScoring
+                ?
+                (<Score>
+                  <ScoreSpan in>{away + 1}</ScoreSpan>
+                  <ScoreSpan out>{away}</ScoreSpan>
+                </Score>)
+                : (<ScoreSpan>{away}</ScoreSpan>)
+              }
+            </Score>
+          </ScoreContainer>
+        </ScoreBox>
+        <Team away>
+          <Name>MAN</Name>
+          <TeamColor color={'red'} />
+        </Team>
+        <Timer started={started}>{time}</Timer>
+      </Main>
+    );
+  }
+}
+
+const Main = styled.div`
+  display: flex;
+  font-family: 'Montserrat', sans-serif;
+  left: 25px;
+  position: absolute;
+  top: 25px;
+`;
+
+const Team = styled.div`
+  align-items: center;
+  background-color: hsla(240, 2%, 82%, .4);
+  border-radius: 0;
+  display: flex;
+  font-weight: 600;
+  height: 28px;
+  justify-content: center;
+  overflow: hidden;
+  width: 66px;
+`;
+
+const Name = styled.div`
+  color: #fff;
+  font-size: 18px;
+  font-weight: 700;
+  height: auto;
+  text-align: center;
+  text-transform: uppercase;
+  width: 62px;
+`;
+
+const ScoreBox = styled.div`
+  display: flex;
+`;
+
+const ScoreContainer = styled.div`
+  overflow: hidden;
+  z-index: 1;
+`;
+
+const Score = styled.div`
+  align-items: center;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  height: 28px;
+  justify-content: center;
+  width: 32px;
+  z-index: 1;
+`;
+
+const ScoreSpan = styled.span`
+  color: #484a4c;
+  font-size: 16px;
+  font-weight: 700;
+  ${p => (p.in || p.out) && css`
+    animation: ${p.in ? numberInAnimation : numberOutAnimation} 1s 0s 1;
+  `}
+`;
+
+const TeamColor = styled.div`
+  background-color: #fff;
+  background-color: ${p => p.color};
+  height: 100%;
+  width: 4px;
+`;
+
+const Timer = styled.div`
+  align-items: center;
+  background-color: #0bd280;
+  border-radius: 0 0 3px 3px;
+  box-shadow: inset 0 0 3px 0 rgba(10, 10, 10, .09);
+  color: #fff;
+  display: flex;
+  font-weight: 700;
+  height: 25px;
+  justify-content: center;
+  left: 50%;
+  overflow: hidden;
+  position: absolute;
+  text-align: center;
+  top: 28px;
+  transform: translate(-50%, ${p => p.started ? '0px' : '-25px'});
+  transition: transform 1s cubic-bezier(.08, .91, .30, 1);
+  width: 64px;
+  z-index: 0;
+`;
+
+const numberInAnimation = keyframes`
+  from {
+    transform: translateY(-28px)
+  }
+  to {
+    transform: translateY(10px)
+  }
+`;
+
+const numberOutAnimation = keyframes`
+  from {
+    transform: translateY(0)
+  }
+  to {
+    transform: translateY(30px)
+  }
+`;
+
+export default Scoreboard;
